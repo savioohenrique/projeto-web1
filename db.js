4be3c59f870cddd2c261dabdbb7edeb4c606ae8d
savioohenrique <saviohenrique.dev@gmail.com>
2021-08-22 20:44:00 -0300

    Ajustando função de adicionar projeto e implementação das funções de atualizar e deletar um projeto

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ async function connect(){
 
 async function selectProjetos(){
     const conn = await connect(); 
-    const sql = 'SELECT p.numero_ano_registro, p.projeto, p.numero_funpec, p.titulo_projeto, a.ambito, p.parceiro, tf.financiamento, cp.classificacao, tcr.tipo_captacao, p.data_inicio, p.data_fim, s.status, p.valor, p.unidade_interessada, p.nome_coordenador, p.unidade_academica_coordenador, p.docentes, p.bolsista_graduacao, p.bolsista_especialista, p.bolsista_mestrado, p.bolsista_doutorado, p.especialista_convidado, p.clt, p.tecnico_ufrn FROM projetos p ' +
+    const sql = 'SELECT p.id, p.numero_ano_registro, p.projeto, p.numero_funpec, p.titulo_projeto, a.ambito, p.parceiro, tf.financiamento, cp.classificacao, tcr.tipo_captacao, p.data_inicio, p.data_fim, s.status, p.valor, p.unidade_interessada, p.nome_coordenador, p.unidade_academica_coordenador, p.docentes, p.bolsista_graduacao, p.bolsista_especialista, p.bolsista_mestrado, p.bolsista_doutorado, p.especialista_convidado, p.clt, p.tecnico_ufrn FROM projetos p ' +
     'JOIN ambito a ON p.ambito_id = a.id ' +
     'JOIN tipo_financiamento tf ON p.tipo_financiamento_id = tf.id ' +
     'JOIN classificacao_projeto cp ON p.classificacao_projeto_id = cp.id ' +
@@ -25,8 +25,71 @@ async function selectProjetos(){
 async function insertProjeto(projeto){
     const conn = await connect();
     const sql = 'INSERT INTO projetos(numero_ano_registro, projeto, numero_funpec, titulo_projeto, ambito_id, parceiro, tipo_financiamento_id, classificacao_projeto_id, tipo_captacao_recurso_id, data_inicio, data_fim, status_id, valor, unidade_interessada, nome_coordenador, unidade_academica_coordenador, docentes, bolsista_graduacao, bolsista_especialista, bolsista_mestrado, bolsista_doutorado, especialista_convidado, clt, tecnico_ufrn) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
-    const values = [customer.nome, customer.idade, customer.uf];
+    const values = [
+        projeto['anoregistro'], 
+        projeto['projeto'], 
+        projeto['numeroFunpec'], 
+        projeto['tituloProjeto'], 
+        projeto['ambito'], 
+        projeto['parceiro'], 
+        projeto['tipoFinanciamento'], 
+        projeto['classificacaoProjeto'], 
+        projeto['tipoCaptacao'], 
+        projeto['dataInicio'], 
+        projeto['dataFim'], 
+        projeto['status'], 
+        projeto['valor'], 
+        projeto['unidade'], 
+        projeto['nomeCoordenador'], 
+        projeto['unidadeAcademicaCoordenador'], 
+        projeto['quantidadeDocentes'], 
+        projeto['quantidadeGraduacao'], 
+        projeto['quantidadeEspecializacao'], 
+        projeto['quantidadeMestrado'], 
+        projeto['quantidadeDoutorado'], 
+        projeto['quantidadeConvidado'], 
+        projeto['quantidadeCLT'], 
+        projeto['quantidadeTecnico']
+    ];
     return await conn.query(sql, values);
 }
 
-module.exports = {selectProjetos, connect, insertProjeto}
+async function updateProjeto(id, projeto){
+    const conn = await connect();
+    const sql = 'UPDATE projetos SET projeto=?, numero_funpec=?, titulo_projeto=?, ambito_id=?, parceiro=?, tipo_financiamento_id=?, classificacao_projeto_id=?, tipo_captacao_recurso_id=?, data_inicio=?, data_fim=?, status_id=?, valor=?, unidade_interessada=?, nome_coordenador=?, unidade_academica_coordenador=?, docentes=?, bolsista_graduacao=?, bolsista_especialista=?, bolsista_mestrado=?, bolsista_doutorado=?, especialista_convidado=?, clt=?, tecnico_ufrn=? WHERE id=?';
+    const values = [
+        projeto['projeto'], 
+        projeto['numeroFunpec'], 
+        projeto['tituloProjeto'], 
+        projeto['ambito'], 
+        projeto['parceiro'], 
+        projeto['tipoFinanciamento'], 
+        projeto['classificacaoProjeto'], 
+        projeto['tipoCaptacao'], 
+        projeto['dataInicio'], 
+        projeto['dataFim'], 
+        projeto['status'], 
+        projeto['valor'], 
+        projeto['unidade'], 
+        projeto['nomeCoordenador'], 
+        projeto['unidadeAcademicaCoordenador'], 
+        projeto['quantidadeDocentes'], 
+        projeto['quantidadeGraduacao'], 
+        projeto['quantidadeEspecializacao'], 
+        projeto['quantidadeMestrado'], 
+        projeto['quantidadeDoutorado'], 
+        projeto['quantidadeConvidado'], 
+        projeto['quantidadeCLT'], 
+        projeto['quantidadeTecnico'],
+        id
+    ];
+    return await conn.query(sql, values);
+}
+
+async function deleteProjeto(id) {
+    const conn = await connect();
+    const sql = 'DELETE FROM projetos WHERE id = ?'
+    return await conn.query(sql, id);
+}
+
+module.exports = {selectProjetos, connect, insertProjeto, updateProjeto, deleteProjeto}
--- index.js
+++ index.js
@@ -1,53 +1,77 @@
 const express = require('express')
-
-const server = express()
+const cors = require('cors')
 
 const db = require('./db')
 
+const server = express()
+
+server.use("/static", express.static("public"))
+
+server.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    server.use(cors());
+    next();
+});
+
 server.use(express.json())
+
+server.get('/', async (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+server.get('/script.js', async (req, res) => {
+    res.sendFile(__dirname + '/public/script.js');
+})
 
 //Retorna todos os projetos
 server.get('/projetos', async (req, res) => {
     console.log('Buscando projetos...')
     const projetos = await db.selectProjetos()
-    console.log(projetos)
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    // console.log(projetos)
     return res.json(projetos)      
 })
 
 //Cria um projeto
 server.post('/projetos', async (req, res) => {
+    const projeto = req.body
+
     console.log('Adicionando projeto...');
+    const result = await db.insertProjeto(projeto);
+    console.log(result);
 
-    const [projeto] = JSON.parse(req.body)
-
-    console.log(projeto)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'POST')
-
-    // console.log('SELECT * FROM CLIENTES');
-    const projetos = await db.selectProjetos();
-    // console.log(projetos);
-
-    // console.log('INSERT INTO projetos');
-    // const result = await db.insertCustomer(projeto);
-    // console.log(result);
-
-    // console.log('SELECT * FROM CLIENTES');
-    // const clientes = await db.selectCustomers();
-    // console.log(clientes);
-
-    // return res.json(projetos)
+    console.log('Buscando projetos...');
+    const projetos = await db.selectCustomers()
+    console.log(projetos);
+    
+    return res.json(projetos)
 })
 
 // Atualizar um projeto
 server.put('/projetos/:index', async (req, res) => {
+    console.log('Atualizando projeto...');
+
+    const id = req.params['index']
+    const projeto = req.body
+
+    const result = await db.updateProjeto(id, projeto)
+    console.log(result)
     
+    return res.json(result)
 })
 
 // Deletar um projeto
 server.delete('/projetos/:index', async (req, res) => {
+    console.log('Deletando projeto...');
+
+    const id = req.params['index']
+
+    const result = await db.deleteProjeto(id)
+    console.log(result)
     
+    return res.json(result)
 })
 
 server.listen(3000)
