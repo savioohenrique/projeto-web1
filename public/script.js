66e60eb3f4f9895ac6be22de67ae788397b1ab9f
savioohenrique <saviohenrique.dev@gmail.com>
2021-08-26 01:46:30 -0300

    Adicionando tabela (Josué) e realizando a ligação com o backend

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+let tabela = '';
+
 let adicionaProjeto = function () {
     console.log("Cadastrando novo projeto...");
 
@@ -80,6 +82,10 @@ let adicionaProjeto = function () {
     adicionaProjetoDB();
 };
 
+
+
+
+
 let adicionaProjetoDB = async function () {
     console.log("Cadastrando novo projeto...");
 
@@ -143,15 +149,19 @@ let adicionaProjetoDB = async function () {
         "descricaoAditivo" : descricaoAditivo.value 
     };
 
-    await postProjeto(JSON.stringify(projeto))
+    console.log(projeto);
+    console.log();
+    console.log(JSON.stringify(projeto));
+
+    await postProjeto('http://localhost:3000/projetos', projeto);
+
 };
 
 let listarProjetos = async function () {
+    console.log('Listando projetos');
     let projetos = await buscarProjetos();
-
-    console.log('Listando projetos')
-    console.log(projetos);
-    console.log();
+    jsonData = projetos;
+    atualizarTabela();
 }
 
 
@@ -172,18 +182,16 @@ async function buscarProjetos() {
 
 };
 
-async function postProjeto(resourceToPost){
-    console.log(resourceToPost)
-
+async function postProjeto(urlToPost, resourceToPost){
     try {
-        let response = await fetch('http://localhost:3000/projetos', {
+        let response = await fetch(urlToPost, {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-type': 'application/json',
-                'Access-Control-Request-Method': 'POST'               
+                'Access-Control-Request-Method': 'POST'                
             },
-            body: resourceToPost
+            body: JSON.stringify(resourceToPost)
         });
         let msg = await response.json();
         console.log(msg ? msg : response);
@@ -192,6 +200,9 @@ async function postProjeto(resourceToPost){
         console.log(error)
     }
 }
+
+
+
 
 let removerProjeto = function () {
     console.log("Removendo projeto...");
@@ -235,3 +246,139 @@ let salvarProjeto = function () {
 };
 
 const NovosProjetos = document.getElementById("projetosNovos");
+
+// fazendo a tabela com json estatico para testes
+var jsonData = [];
+
+const btnGroupStr = '<div class="btn-group"><a id="btnEdtitar" class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#myModal" href=#/>' + 'Editar' + '</a>'
++ '<a id="btnExcluir" class="btn btn-danger btn-sm" href=#/>' + 'Excluir' + '</a>' + "</div>";
+
+function atualizarTabela(){
+    table = $('#table').DataTable();
+    table.destroy();
+
+    preencherTabela();
+}
+
+function preencherTabela() {
+    console.log("Preenchendo tabela");
+    tabela = $('#table').DataTable({
+        data: jsonData,
+        scrollY: "500px",
+        scrollX: true,
+        scrollCollapse: true,
+        // dom: 'Bfrtip',
+        // buttons: [
+        //     {
+        //         text: 'Adicionar',
+        //         action: function ( e, dt, node, config ) {
+        //             alert( 'Button 1 clicked on' );
+        //         }
+        //     }
+        // ],
+        "columnDefs": [ {
+            // targets: [0, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 22, 23], visible: false,
+            // { targets: '_all', visible: true },
+            // "width": "40%",
+            // targets: '_all', visible: false, width: '20px',
+            "targets": 0,
+            // "data": null,
+            "defaultContent": btnGroupStr,
+            // "mRender": function(data, type, full) {
+            //     return '<div class="btn-group"><a id="btnEdtitar" class="btn btn-info btn-sm" href=#/' + full[0] + '>' + 'Editar' + '</a>'
+            //     + '<a id="btnExcluir" class="btn btn-danger btn-sm" href=#/' + full[0] + '>' + 'Excluir' + '</a>' + "</div>";
+            // }
+            // "render": function(data, type, full, meta){
+            //     if (full.activated) {
+            //         return '<button class="btn btn-mini btn-primary pull-right"> Enabled</button>';
+            //     } else {
+            //         return '<button class="btn btn-mini btn-danger pull-right"> Disabled</button>';
+            //     }
+            //  }
+        }],
+        "columns": [
+            { data: "", /*width: "50px"*/ }, // espaco necessario para a coluna dos botoes
+            { data: "numero_ano_registro" },
+            { data: "projeto" },
+            { data: "numero_funpec" },
+            { data: "titulo_projeto" },
+            { data: "ambito" },
+            { data: "parceiro" },
+            { data: "financiamento" },
+            { data: "classificacao" },
+            { data: "tipo_captacao" },
+            { data: "data_inicio" },
+            { data: "data_fim" },
+            { data: "status" },
+            { data: "valor" },
+            { data: "unidade_interessada" },
+            { data: "nome_coordenador" },
+            { data: "unidade_academica_coordenador" },
+            { data: "docentes" },
+            { data: "bolsista_graduacao" },
+            { data: "bolsista_especialista" },
+            { data: "bolsista_mestrado" },
+            { data: "bolsista_doutorado" },
+            { data: "especialista_convidado" },
+            { data: "clt" },
+            { data: "tecnico_ufrn" },
+        ],
+        
+        // initComplete: function() {
+        //     this.api().columns().every( function() {
+        //         let column = this;
+        //         let select = $('<select><option value=""></option></select>')
+        //             .appendTo( $(column.footer()).empty() )
+        //             .on('change', function() {
+        //                 let val = $.fn.dataTable.util.escapeRegex(
+        //                     $(this).val()
+        //                 );
+    
+        //                 column
+        //                     .search( val ? '^' + val + '$' : '', true, true)
+        //                     .draw();
+        //             })
+        //         column.data().unique().sort().each( (d, j) => {
+        //             select.append( '<option value=" '+d+' ">'+d+'</option>')
+        //         });
+        //     });
+        // },
+    });
+
+    
+}
+
+$(document).ready(function() {
+    listarProjetos();
+
+    preencherTabela();    
+
+    // $('#table tbody').on( 'click', '#btnEdtitar', function () {
+    //     var rowData = tabela.fnGetData(0).length;
+    //     alert( data[0] +"'s lenght is: "+ rowData );
+    // });
+    $('#table tbody').on( 'click', '#btnEdtitar', function () {
+        // var rowData = tabela.fnGetData(0).length;
+        // alert( data[0] +"'s lenght is: "+ rowData );
+        // var d = tabela.row(this).data();
+
+        // alert(tabela.row(this).data(d).draw());
+        // alert( "funcionou" );
+    });
+    $('#table tbody').on( 'click', '#btnExcluir', function () {
+        // var rowData = tabela.fnGetData(0).length;
+        // alert( data[0] +"'s lenght is: "+ rowData );
+        alert( "Deseja Realmente Excluir ?" );
+    });
+    // tabela.buttons( 1, null ).container().appendTo(
+    //     tabela.table().container()
+    // );
+});
+
+// $('#table').DataTable({
+//     "data": jsonData,
+//     "columns": [
+//       { "data": "meta.type" },
+//       { "data": "meta.version" }
+//     ]
+// });
